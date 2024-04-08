f1972e40750c6191a75d5ae0cb73234926222ac5
Andreasgura <andre-56@hotmail.it>
2024-04-08 17:01:02 +0200

    emailList with buffer

--- js/script.js
+++ js/script.js
@@ -2,16 +2,33 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      
+      emaiAddress: null,
+      emailList: [],
+      listLength: 10
     }
   },
   methods: {
-    
+    getRndEmail() {
+      for (let index = 0; index < this.listLength; index++) {
+         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+          console.log(response.data);
+
+          const email = response.data.response
+          this.emailList.push(email)
+          console.log(this.emailList);
+        }
+
+        )
+      }
+
+
+    }
+
   },
   computed: {
-    
+
   },
   mounted() {
-
+    this.getRndEmail()
   }
 }).mount('#app');
